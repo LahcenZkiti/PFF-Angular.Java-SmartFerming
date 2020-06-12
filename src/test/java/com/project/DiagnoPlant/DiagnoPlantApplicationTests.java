fba08b0ed83b5fa06bc8e588e4200d312540d96e
zkitilahcen-cyber <atdheserveur@gmail.com>
2020-06-12 06:42:43 +0100

    include dist in static and add jar

--- src/test/java/com/project/DiagnoPlant/DiagnoPlantApplicationTests.java
+++ src/test/java/com/project/DiagnoPlant/DiagnoPlantApplicationTests.java
@@ -1,13 +1,8 @@
 package com.project.DiagnoPlant;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootTest
+@SpringBootApplication
 class DiagnoPlantApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
