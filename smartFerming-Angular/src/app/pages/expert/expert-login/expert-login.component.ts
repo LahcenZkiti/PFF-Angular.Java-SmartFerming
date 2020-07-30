1aca3b226ab28d96db4872971233487c4fc31a0d
zkitilahcen-cyber <atdheserveur@gmail.com>
2020-07-30 19:44:14 +0100

    added login expert

--- smartFerming-Angular/src/app/pages/expert/expert-login/expert-login.component.ts
+++ smartFerming-Angular/src/app/pages/expert/expert-login/expert-login.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from 'src/app/services/authentication.service';
+import { TokenStorageService } from 'src/app/services/token-storage.service';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'app-expert-login',
@@ -7,9 +12,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExpertLoginComponent implements OnInit {
 
-  constructor() { }
+  form: any = {};
+  isLoggedIn = false;
+  isLoginFailed = false;
+  errorMessage = '';
+  roles: string[] = [];
+
+  constructor(private authService:AuthenticationService,
+    private tokenStorage: TokenStorageService,
+    private router:Router,
+    private toastr: ToastrService) { }
 
   ngOnInit(): void {
+    if (this.tokenStorage.getToken()) {
+      this.isLoggedIn = true;
+      this.roles = this.tokenStorage.getUser().roles;
+    }
+  }
+
+  onlogin(){
+    this.authService.expertLogin(this.form).subscribe(
+      (user:User) => {
+        console.log(user);
+        this.tokenStorage.saveToken(user.accessToken);
+        this.tokenStorage.saveUser(user);
+        this.tokenStorage.saveUserType(user.userType);
+
+        this.isLoginFailed = false;
+        this.isLoggedIn = true;
+        this.roles = this.tokenStorage.getUser().roles;
+        console.log('login successful');
+        this.toastr.success("login successful");
+        console.log(this.tokenStorage.getUser());
+        this.reloadPage();
+      },
+
+      err => {
+        console.log(err);
+        this.errorMessage = err.error.message;
+        this.toastr.error("Login failed");
+        this.isLoginFailed = true;
+        
+      }
+    )
+  }
+
+  reloadPage() {
+    window.location.reload();
   }
 
 }
