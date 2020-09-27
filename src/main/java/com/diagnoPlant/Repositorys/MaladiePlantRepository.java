860cd44e2520d6a8486dea719d5ec14c01ce1dba
zkitilahcen-cyber <atdheserveur@gmail.com>
2020-09-27 23:44:15 +0100

    added Jpa specification Executor

--- src/main/java/com/diagnoPlant/Repositorys/MaladiePlantRepository.java
+++ src/main/java/com/diagnoPlant/Repositorys/MaladiePlantRepository.java
@@ -5,10 +5,11 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.diagnoPlant.Models.MaladiePlante;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MaladiePlantRepository extends JpaRepository<MaladiePlante, Long> {
+public interface MaladiePlantRepository extends JpaRepository<MaladiePlante, Long>, JpaSpecificationExecutor<MaladiePlante> {
 
 	List<MaladiePlante> findByNomMaladieContains(String nomMaladie);
 
