331da1d70229ed18e233f82b8606cacd11300f50
zkitilahcen-cyber <atdheserveur@gmail.com>
2020-09-30 23:14:21 +0100

    edit service expert

--- src/main/java/com/diagnoPlant/services/ExpertService.java
+++ src/main/java/com/diagnoPlant/services/ExpertService.java
@@ -1,2 +1,61 @@
-package com.diagnoPlant.services;public class ExpertService {
+package com.diagnoPlant.services;
+
+import com.diagnoPlant.Models.Expert;
+import org.springframework.data.domain.Sort;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ExpertService {
+
+    /*
+    * List des experts
+    * @return List<Expert>
+    * */
+    Optional<List<Expert>> findAll(Sort sort);
+
+    /*
+     * trouver un expert par son id
+     * @return expert
+     * */
+    Optional<Expert> findById(Long id);
+
+    /*
+     * trouver un expert par son email
+     * @param email
+     * */
+    public Optional<Expert> findByEmail(String email);
+
+
+    /*
+     * trouver un expert par son username
+     * @param email
+     * */
+    public Optional<Expert> findByUsername(String username);
+
+    /*
+    * Ajouter un expert
+    * @param expert
+    * */
+    public Optional<Expert> save(Expert expert);
+
+
+
+    /*
+     * modifier un expert
+     * @param expert
+     * */
+    public Optional<Expert> update(Expert expert);
+
+    /*
+     * Supprimer un expert
+     * */
+    void deletById(Long id);
+
+    /*
+     * Supprimer tout les experts
+     * @return expert
+     * */
+    public void deleteAll();
+
 }
