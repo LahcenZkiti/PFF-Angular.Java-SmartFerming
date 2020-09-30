8d41edf94f02ad416f688faec0679ac09f834d64
zkitilahcen-cyber <atdheserveur@gmail.com>
2020-09-30 19:31:50 +0100

    added attr of review

--- src/main/java/com/diagnoPlant/Models/Review.java
+++ src/main/java/com/diagnoPlant/Models/Review.java
@@ -1,2 +1,38 @@
-package com.diagnoPlant.Models;public class Review {
+package com.diagnoPlant.Models;
+
+import javax.persistence.Id;
+
+public class Review {
+
+    @Id
+    private Long id;
+    private String content;
+    private int agricultureId;
+
+    public Review() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public int getAgricultureId() {
+        return agricultureId;
+    }
+
+    public void setAgricultureId(int agricultureId) {
+        this.agricultureId = agricultureId;
+    }
 }
