53a3038b277fd045d06119d82848ca43bc3c5bc2
zkitilahcen-cyber <atdheserveur@gmail.com>
2020-09-30 19:32:39 +0100

    ReviewRepository

--- src/main/java/com/diagnoPlant/Repositorys/ReviewRepository.java
+++ src/main/java/com/diagnoPlant/Repositorys/ReviewRepository.java
@@ -1,2 +1,7 @@
-package com.diagnoPlant.Repositorys;public interface ReviewRepository {
+package com.diagnoPlant.Repositorys;
+
+import com.diagnoPlant.Models.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReviewRepository extends JpaRepository<Review, Long> {
 }
