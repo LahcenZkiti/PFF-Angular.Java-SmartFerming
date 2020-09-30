4b54722f45a5f852e17c43b27955b77d8c396a35
zkitilahcen-cyber <atdheserveur@gmail.com>
2020-09-30 21:38:08 +0100

    added expertController

--- src/main/java/com/diagnoPlant/Controllers/ExpertController.java
+++ src/main/java/com/diagnoPlant/Controllers/ExpertController.java
@@ -1,2 +1,15 @@
-package com.diagnoPlant.Controllers;public class ExpertController {
+package com.diagnoPlant.Controllers;
+
+import com.diagnoPlant.Repositorys.ExpertRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/expert")
+public class ExpertController {
+    @Autowired
+    ExpertRepository expertRepository;
+
+
 }
