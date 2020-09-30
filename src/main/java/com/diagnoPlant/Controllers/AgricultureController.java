f65e0dba096fc2fab0432ab82f530fbc4c7a986d
zkitilahcen-cyber <atdheserveur@gmail.com>
2020-09-30 21:37:33 +0100

    added agricontroller

--- src/main/java/com/diagnoPlant/Controllers/AgricultureController.java
+++ src/main/java/com/diagnoPlant/Controllers/AgricultureController.java
@@ -1,2 +1,16 @@
-package com.diagnoPlant.Controllers;public class AgricultureController {
+package com.diagnoPlant.Controllers;
+
+import com.diagnoPlant.Repositorys.AgricultureRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/agriculture")
+public class AgricultureController {
+
+    @Autowired
+    AgricultureRepository agricultureRepository;
+
+
 }
