28e8aa4b1c6206eda2342d4dc92fd151b7285fed
zkitilahcen-cyber <atdheserveur@gmail.com>
2020-10-22 01:34:03 +0100

    update MaladieService

--- smartFerming-Angular/src/app/services/maladies.service.ts
+++ smartFerming-Angular/src/app/services/maladies.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { MaladiePlante } from '../models/maladies-plant';
 import { Observable } from 'rxjs';
 
-const API_URL = 'http://localhost:8080/api/mld/';
+const API_URL = 'http://localhost:8080/api/mld';
 
 
 /**
@@ -22,51 +22,56 @@ export class MaladiesService {
 
   /**
    * Creates an instance of maladies service.
-   * @param http 
+   * @param http
    */
   constructor(private http: HttpClient) { }
 
   /**
    * Adds one
-   * @param maladie 
-   * @returns one 
+   * @param maladie
+   * @returns one
    */
   addOne(maladie: MaladiePlante): Observable<MaladiePlante> {
-    return this.http.post<MaladiePlante>(API_URL + 'addMaladie' , maladie);
+    return this.http.post<MaladiePlante>(`${API_URL}`, maladie);
   }
 
   /**
    * Finds all
-   * @returns all 
+   * @returns all
    */
-  findAll(): Observable<MaladiePlante> {
-    return this.http.get<MaladiePlante>(API_URL + 'listMaladies');
+  findAll(): Observable<MaladiePlante[]> {
+    return this.http.get<MaladiePlante[]>(`${API_URL}`);
   }
 
   /**
    * Finds by id
-   * @param id 
-   * @returns by id 
+   * @param id
+   * @returns by id
    */
   findById(id: number): Observable<MaladiePlante> {
-    return this.http.get<MaladiePlante>(API_URL + 'listMaladies/' + id);
+    return this.http.get<MaladiePlante>(`${API_URL}/${id}`);
   }
 
   /**
    * Deletes maladies service
-   * @param id 
-   * @returns delete 
+   * @param id
+   * @returns delete
    */
   delete(id: number): Observable<MaladiePlante> {
-    return this.http.delete<MaladiePlante>(API_URL + 'listMaladies/' + id)
+    return this.http.delete<MaladiePlante>(`${API_URL}/${id}`)
   }
 
   /**
    * Updates maladies service
-   * @param maladie 
-   * @returns update 
+   * @param maladie
+   * @returns update
    */
   update(maladie: MaladiePlante ): Observable<MaladiePlante> {
-    return this.http.put<MaladiePlante>(API_URL + 'listMaladies/' + maladie.id, maladie)
+    return this.http.put<MaladiePlante>(`${API_URL}/ ${maladie.idMaladiePlante}`, maladie)
   }
+
+  search(maladie: MaladiePlante): Observable<MaladiePlante>{
+    return this.http.get<MaladiePlante>(`${API_URL}/nomMLD${maladie}`);
+  }
+
 }
